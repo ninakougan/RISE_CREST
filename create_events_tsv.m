9cf961c807ad926bf7d0ecea2482afb2f921af5e
ninakougan <ninakougan@northwestern.edu>
2023-12-08 17:16:16 -0600

    12/8/23 edits (we made some good progress today)

--- create_events_tsv.m
+++ create_events_tsv.m
@@ -4,22 +4,19 @@
 
 basedir = '/Users/ninakougan/Documents/acnl/rise_crest/progress_reports/'; %make this wherever your bids dataset is%
 
-%% MID events.tsv files 
-MIDfnames = filenames(fullfile(basedir,'sub-*/ses-1/beh/3_MID*txt')); %swap session here%
-
+%% Events.tsv files that don't make my brain hurt
+MIDfnames = filenames(fullfile(basedir,'test_bids/sub-*/ses-1/beh/3_MID*txt')); %swap session here%
 txt = readtable(MIDfnames{1});
 
-%pull PID and session ID
 pid = txt.Var2(matches(txt.Var1, 'Subject'));
 pid = string(pid(1));
 ses = txt.Var2(matches(txt.Var1, 'Session'));
 ses = string(ses(1));
 
 %RUN 1
-cue_onset1 = (txt.Var2(matches(txt.Var1,'Run1Cue.OnsetTime'))) - (txt.Var2(matches(txt.Var1, 'Run1Fix.OnsetTime'))) ./ 1000;
-tgt_onset1 = txt.Var2(matches(txt.Var1, 'Run1Tgt.OnsetTime')) ./1000; %onset for motor contrast
-fbk_on1 = txt.Var2(matches(txt.Var1,'Run1Fbk.OnsetTime'));
-fbk_on1 = (fbk_on1 - txt.Var2(strcmp(txt.Var1,'Run1Fix.OnsetTime'))) ./ 1000; 
+cue_onset1 = ((txt.Var2(matches(txt.Var1,'Run1Cue.OnsetTime'))) - (txt.Var2(matches(txt.Var1, 'Run1Fix.OnsetTime')))) ./ 1000;
+tgt_onset1 = (txt.Var2(matches(txt.Var1, 'Run1Tgt.OnsetTime')) - (txt.Var2(matches(txt.Var1, 'Run1Fix.OnsetTime')))) ./1000; %onset for motor contrast
+fbk_on1 = (txt.Var2(matches(txt.Var1,'Run1Fbk.OnsetTime')) - (txt.Var2(matches(txt.Var1,'Run1Fix.OnsetTime')))) ./ 1000;
 
 cue_dur1 = txt.Var2(matches(txt.Var1,'Run1Cue.Duration')) ./ 1000; %same for all cues across runs, should be 2
 tgt_dur1 = txt.Var2(matches(txt.Var1,'Run1Tgt.Duration'));
@@ -38,69 +35,141 @@ trial_type1 = replace(trial_type1,'4-','Lose $5.00');
 trial_type1 = replace(trial_type1,'5-','Lose $1.50');
 trial_type1 = replace(trial_type1,'6-','Lose $0.00');
 
-str1 = 'Anticipation'
-anticipation = {str1};
-anticipation = string(repmat(anticipation,48,1));
-
-str2 = 'Motor'
-motor = {str2};
-motor = string(repmat(motor,48,1));
-
-str3 = 'Outcome'
-outcome = {str3};
-outcome = string(repmat(outcome,48,1));
-
-null = repmat(NaN, 48, 1);
-
 %compile variables into events.tsv file
-onset1 = [cue_onset1;tgt_onset1;fbk_on1];
-duration1 = [cue_dur1;tgt_dur1;fbk_dur1];
-trial1 = [trial_type1;trial_type1;trial_type1];
-response_time1 = [null;rt1;null];
-accuracy1 = [null; null; acc1];
-contrast_type = [anticipation;motor;outcome] ;
-%fix = 
+mid_events1 = array2table([cue_onset1, cue_dur1, trial_type1, tgt_onset1, tgt_dur1, rt1, fbk_on1, fbk_dur1, acc1])
+mid_events1.Properties.VariableNames = {'onset', 'duration', 'trial_type',...
+    'target_onset', 'target_duration', 'reaction_time', 'feedback_onset', 'feedback_duration', 'accuracy'}
 
-mid_events1 = array2table([onset1, duration1, trial1, response_time1, accuracy1, contrast_type]);
-mid_events1.Properties.VariableNames = {'onset', 'duration', 'trial_type', 'response_time', 'accuracy', 'contrast_type'};
-mid_txt = fullfile(basedir, "test_bids/", strcat('sub-',pid,'/ses-',ses,'/func/sub-',pid,'_ses-',ses,'_task-mid-01_events.txt'));
+mid_txt = fullfile(basedir, "test_bids/", strcat('sub-',pid,'/ses-',ses,'/func/sub-',pid,'_ses-',ses,'_task-mid_run-01_events.txt'));
     writetable(mid_events1,mid_txt,'delimiter','tab')
 
-%RUN 2 (Same as above)
-%columns to include for BIDS
-cue_onset2 = (txt.Var2(matches(txt.Var1,'Run2Cue.OnsetTime'))) - (txt.Var2(matches(txt.Var1, 'Run2Fix.OnsetTime'))) ./ 1000;
-tgt_onset2 = txt.Var2(matches(txt.Var1, 'Run2Tgt.OnsetTime')) ./1000; %onset for motor contrast
-fbk_on2 = txt.Var2(matches(txt.Var1,'Run2Fbk.OnsetTime'));
-fbk_on2 = (fbk_on2 - txt.Var2(strcmp(txt.Var1,'Run2Fix.OnsetTime'))) ./ 1000; 
+%RUN 2
+cue_onset2 = ((txt.Var2(matches(txt.Var1,'Run2Cue.OnsetTime'))) - (txt.Var2(matches(txt.Var1, 'Run2Fix.OnsetTime')))) ./ 1000;
+tgt_onset2 = (txt.Var2(matches(txt.Var1, 'Run2Tgt.OnsetTime')) - (txt.Var2(matches(txt.Var1, 'Run2Fix.OnsetTime')))) ./1000
+fbk_on2 = (txt.Var2(matches(txt.Var1,'Run2Fbk.OnsetTime')) - (txt.Var2(matches(txt.Var1,'Run2Fix.OnsetTime')))) ./ 1000;
 
 cue_dur2 = txt.Var2(matches(txt.Var1,'Run2Cue.Duration')) ./ 1000; %same for all cues across runs, should be 2
 tgt_dur2 = txt.Var2(matches(txt.Var1,'Run2Tgt.Duration'));
 fbk_dur2 = txt.Var2(matches(txt.Var1,'Run2Fbk.Duration')) ./ 1000; %same for all cues across runs, should be 2
 
 rt2 = txt.Var2(matches(txt.Var1, 'Run2Tgt.RT')) ./ 1000;
+reward = txt.Var2(matches(txt.Var1, 'Rwd'));%this is for both runs, need to splitsky
 reward2 = reward(49:96);
 acc2 = txt.Var2(matches(txt.Var1, 'Run2Tgt.ACC'));
 
 trial_type2 = strcat(string(txt.Var2(matches(txt.Var1,'RunList2'))),'-'); %maybe one day I can figure out how to code this without the dash... today is just not that day!
-trial_type2 = replace(trial_type2,'1-','Win $5.00');
-trial_type2 = replace(trial_type2,'2-','Win $1.50');
-trial_type2 = replace(trial_type2,'3-','Win $0.00');
-trial_type2 = replace(trial_type2,'4-','Lose $5.00');
-trial_type2 = replace(trial_type2,'5-','Lose $1.50');
-trial_type2 = replace(trial_type2,'6-','Lose $0.00');
+trial_type2 = replace(trial_type1,'1-','Win $5.00');
+trial_type2 = replace(trial_type1,'2-','Win $1.50');
+trial_type2 = replace(trial_type1,'3-','Win $0.00');
+trial_type2 = replace(trial_type1,'4-','Lose $5.00');
+trial_type2 = replace(trial_type1,'5-','Lose $1.50');
+trial_type2 = replace(trial_type1,'6-','Lose $0.00');
 
 %compile variables into events.tsv file
-onset2 = [cue_onset2;tgt_onset2;fbk_on2];
-duration2 = [cue_dur2;tgt_dur2;fbk_dur2];
-trial2 = [trial_type2;trial_type2;trial_type2];
-response_time2 = [null;rt2;null];
-accuracy2 = [null; null; acc2];
-%fix = 
+mid_events2 = array2table([cue_onset2, cue_dur2, trial_type2, tgt_onset2, tgt_dur2, rt2, fbk_on2, fbk_dur2, acc2])
+mid_events2.Properties.VariableNames = {'onset', 'duration', 'trial_type',...
+    'target_onset', 'target_duration', 'reaction_time', 'feedback_onset', 'feedback_duration', 'accuracy'}
 
-mid_events2 = array2table([onset2, duration2, trial2, response_time2, accuracy2, contrast_type]);
-mid_events2.Properties.VariableNames = {'onset', 'duration', 'trial_type', 'response_time', 'accuracy', 'contrast_type'};
-mid_txt = fullfile(basedir, "test_bids/", strcat('sub-',pid,'/ses-',ses,'/func/sub-',pid,'_ses-',ses,'_task-mid-02_events.txt'));
+mid_txt = fullfile(basedir, "test_bids/", strcat('sub-',pid,'/ses-',ses,'/func/sub-',pid,'_ses-',ses,'_task-mid_run-02_events.txt'));
     writetable(mid_events2,mid_txt,'delimiter','tab')
+
+% %% MID events.tsv files 
+% MIDfnames = filenames(fullfile(basedir,'sub-*/ses-1/beh/3_MID*txt')); %swap session here%
+% 
+% txt = readtable(MIDfnames{1});
+% 
+% %pull PID and session ID
+% pid = txt.Var2(matches(txt.Var1, 'Subject'));
+% pid = string(pid(1));
+% ses = txt.Var2(matches(txt.Var1, 'Session'));
+% ses = string(ses(1));
+% 
+% %RUN 1
+% cue_onset1 = (txt.Var2(matches(txt.Var1,'Run1Cue.OnsetTime'))) - (txt.Var2(matches(txt.Var1, 'Run1Fix.OnsetTime'))) ./ 1000;
+% tgt_onset1 = txt.Var2(matches(txt.Var1, 'Run1Tgt.OnsetTime')) ./1000; %onset for motor contrast
+% fbk_on1 = txt.Var2(matches(txt.Var1,'Run1Fbk.OnsetTime'));
+% fbk_on1 = (fbk_on1 - txt.Var2(strcmp(txt.Var1,'Run1Fix.OnsetTime'))) ./ 1000; 
+% 
+% cue_dur1 = txt.Var2(matches(txt.Var1,'Run1Cue.Duration')) ./ 1000; %same for all cues across runs, should be 2
+% tgt_dur1 = txt.Var2(matches(txt.Var1,'Run1Tgt.Duration'));
+% fbk_dur1 = txt.Var2(matches(txt.Var1,'Run1Fbk.Duration')) ./ 1000; %same for all cues across runs, should be 2
+% 
+% rt1 = txt.Var2(matches(txt.Var1, 'Run1Tgt.RT')) ./ 1000;
+% reward = txt.Var2(matches(txt.Var1, 'Rwd'));%this is for both runs, need to splitsky
+% reward1 = reward(1:48);
+% acc1 = txt.Var2(matches(txt.Var1, 'Run1Tgt.ACC'));
+% 
+% trial_type1 = strcat(string(txt.Var2(matches(txt.Var1,'RunList1'))),'-'); %maybe one day I can figure out how to code this without the dash... today is just not that day!
+% trial_type1 = replace(trial_type1,'1-','Win $5.00');
+% trial_type1 = replace(trial_type1,'2-','Win $1.50');
+% trial_type1 = replace(trial_type1,'3-','Win $0.00');
+% trial_type1 = replace(trial_type1,'4-','Lose $5.00');
+% trial_type1 = replace(trial_type1,'5-','Lose $1.50');
+% trial_type1 = replace(trial_type1,'6-','Lose $0.00');
+% 
+% str1 = 'Anticipation'
+% anticipation = {str1};
+% anticipation = string(repmat(anticipation,48,1));
+% 
+% str2 = 'Motor'
+% motor = {str2};
+% motor = string(repmat(motor,48,1));
+% 
+% str3 = 'Outcome'
+% outcome = {str3};
+% outcome = string(repmat(outcome,48,1));
+% 
+% null = repmat(NaN, 48, 1);
+% 
+% %compile variables into events.tsv file
+% onset1 = [cue_onset1;tgt_onset1;fbk_on1];
+% duration1 = [cue_dur1;tgt_dur1;fbk_dur1];
+% trial1 = [trial_type1;trial_type1;trial_type1];
+% response_time1 = [null;rt1;null];
+% accuracy1 = [null; null; acc1];
+% contrast_type = [anticipation;motor;outcome] ;
+% %fix = 
+% 
+% mid_events1 = array2table([onset1, duration1, trial1, response_time1, accuracy1, contrast_type]);
+% mid_events1.Properties.VariableNames = {'onset', 'duration', 'trial_type', 'response_time', 'accuracy', 'contrast_type'};
+% mid_txt = fullfile(basedir, "test_bids/", strcat('sub-',pid,'/ses-',ses,'/func/sub-',pid,'_ses-',ses,'_task-mid_run-01_events.txt'));
+%     writetable(mid_events1,mid_txt,'delimiter','tab')
+% 
+% %RUN 2 (Same as above)
+% %columns to include for BIDS
+% cue_onset2 = (txt.Var2(matches(txt.Var1,'Run2Cue.OnsetTime'))) - (txt.Var2(matches(txt.Var1, 'Run2Fix.OnsetTime'))) ./ 1000;
+% tgt_onset2 = txt.Var2(matches(txt.Var1, 'Run2Tgt.OnsetTime')) ./1000; %onset for motor contrast
+% fbk_on2 = txt.Var2(matches(txt.Var1,'Run2Fbk.OnsetTime'));
+% fbk_on2 = (fbk_on2 - txt.Var2(strcmp(txt.Var1,'Run2Fix.OnsetTime'))) ./ 1000; 
+% 
+% cue_dur2 = txt.Var2(matches(txt.Var1,'Run2Cue.Duration')) ./ 1000; %same for all cues across runs, should be 2
+% tgt_dur2 = txt.Var2(matches(txt.Var1,'Run2Tgt.Duration'));
+% fbk_dur2 = txt.Var2(matches(txt.Var1,'Run2Fbk.Duration')) ./ 1000; %same for all cues across runs, should be 2
+% 
+% rt2 = txt.Var2(matches(txt.Var1, 'Run2Tgt.RT')) ./ 1000;
+% reward2 = reward(49:96);
+% acc2 = txt.Var2(matches(txt.Var1, 'Run2Tgt.ACC'));
+% 
+% trial_type2 = strcat(string(txt.Var2(matches(txt.Var1,'RunList2'))),'-'); %maybe one day I can figure out how to code this without the dash... today is just not that day!
+% trial_type2 = replace(trial_type2,'1-','Win $5.00');
+% trial_type2 = replace(trial_type2,'2-','Win $1.50');
+% trial_type2 = replace(trial_type2,'3-','Win $0.00');
+% trial_type2 = replace(trial_type2,'4-','Lose $5.00');
+% trial_type2 = replace(trial_type2,'5-','Lose $1.50');
+% trial_type2 = replace(trial_type2,'6-','Lose $0.00');
+% 
+% %compile variables into events.tsv file
+% onset2 = [cue_onset2;tgt_onset2;fbk_on2];
+% duration2 = [cue_dur2;tgt_dur2;fbk_dur2];
+% trial2 = [trial_type2;trial_type2;trial_type2];
+% response_time2 = [null;rt2;null];
+% accuracy2 = [null; null; acc2];
+% %fix = 
+% 
+% mid_events2 = array2table([onset2, duration2, trial2, response_time2, accuracy2, contrast_type]);
+% mid_events2.Properties.VariableNames = {'onset', 'duration', 'trial_type', 'response_time', 'accuracy', 'contrast_type'};
+% mid_txt = fullfile(basedir, "test_bids/", strcat('sub-',pid,'/ses-',ses,'/func/sub-',pid,'_ses-',ses,'_task-mid_run-02_events.txt'));
+%     writetable(mid_events2,mid_txt,'delimiter','tab')
 
 %% CHATROOM TASK
 CRfnames_txt = filenames(fullfile(basedir,'test_bids/sub-*/ses-1/beh/chzc*txt')); %swap session here% 
--- extract_timing.m
+++ extract_timing.m
@@ -1,17 +1,50 @@
+basedir = '/Users/ninakougan/Documents/acnl/rise_crest/progress_reports/events';
 
 % load in tsv file for events
-t = readtable("sub-50001_ses-1_task-mid_run-02_events.tsv", "FileType","text",'Delimiter', '\t');
+txt = readtable("sub-50001_ses-1_task-mid_run-01_events.txt", "FileType","text",'Delimiter', '\t');
 
 % sort based on events
-antgainidx = contains(t.type,"Win $5.00") | contains(t.type,"Win $1.50");
-antgainzeroidx = contains(t.type,"Win $0.00");
-antlossidx = contains(t.type,"Lose $5.00") | contains(t.type,"Lose $1.50");
-antlosszeroidx = contains(t.type,"Lose $0.00");
+gainidx = contains(txt.trial_type,"Win $5.00") | contains(txt.trial_type,"Win $1.50");
+gainzeroidx = contains(txt.trial_type,"Win $0.00");
+lossidx = contains(txt.trial_type,"Lose $5.00") | contains(txt.trial_type,"Lose $1.50");
+losszeroidx = contains(txt.trial_type,"Lose $0.00");
 
-onsets{1} = t.onset(antgainidx);
+onsets{1} = txt.onset(gainidx);
 durations{1} = ones(1,length(onsets)) .* 4;
 names{1} = {'GainAnticipation'};
 
-% add all trial types
-% need to pull motor onsets, duration will be 2. 
-% save onsets durations names in file that corresponds to each sub.
+onsets{2} = txt.onset(gainzeroidx);
+durations{2} = ones(1,length(onsets)) .* 4;
+names{2} = {'GainZeroAnticipation'};
+
+onsets{3} = txt.onset(lossidx);
+durations{3} = ones(1,length(onsets)) .* 4;
+names{3} = {'LossAnticipation'};
+
+onsets{4} = txt.onset(losszeroidx);
+durations{4} = ones(1,length(onsets)) .* 4;
+names{4} = {'LossZeroAnticipation'};
+
+onsets{5} = txt.feedback_onset(gainidx);
+durations{5} = ones(1,length(onsets)) .* 4;
+names{5} = {'GainOutcome'};
+
+onsets{6} = txt.feedback_onset(gainzeroidx);
+durations{6} = ones(1,length(onsets)) .* 4;
+names{6} = {'GainZeroOutcome'};
+
+onsets{7} = txt.feedback_onset(lossidx);
+durations{7} = ones(1,length(onsets)) .* 4;
+names{7} = {'LossOutcome'};
+
+onsets{8} = txt.feedback_onset(losszeroidx);
+durations{8} = ones(1,length(onsets)) .* 4;
+names{8} = {'LossZeroOutcome'};
+
+onsets{9} = txt.target_onset;
+durations{9} = ones(1,length(onsets)) .* 2;
+names{9} = {'Motor'};
+
+%save onsets, durations, and names to .mat file
+temp_file_name = strcat(num2str(curr_table.PID(trial_ind1)),pid{sub}.mat');
+    save(fullfile(savedir, temp_file_name), 'onsets', 'names', 'durations');
