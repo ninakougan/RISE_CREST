bff418115eeae6d7871acae172577d07c04f096c
ninakougan <ninakougan@northwestern.edu>
2024-08-27 16:43:29 -0500

    smoothing updates

--- preprocessing/batch_quest_smooth.m
+++ preprocessing/batch_quest_smooth.m
@@ -1,65 +1,59 @@
 scriptdir = '/home/nck1870/repos/RISE_CREST';
-
 repodir = '/home/nck1870/repos';
 
-% next is where the preprocessed data is
+% Directory containing the preprocessed data
 directories = '/projects/b1108/studies/rise/data/processed/neuroimaging/fmriprep/ses-1';
 
-% What run of your task are you looking at?
+% Task run and session parameters
 run = 1;
-% What session appears in your raw filenames when in BIDS format?
 ses = 1;
-% Do you want to overwrite previously estimated first levels or just add to
-% what you have? 1 overwrites, 0 adds
+
+% Overwrite control: 1 overwrites, 0 adds to existing data
 overwrite = 0;
-% Last thing is janky but bear with me. How long are your participant ID's?
-% i.e. 10234 would correspond with a 5 for this variable
+
+% Length of participant IDs (e.g., 10234 has length 5)
 ID_length = 5;
 
-%keyboard
-file_list = filenames(fullfile(directories,strcat('*/ses-',num2str(ses),'/func/sub*chatroom_run-0',num2str(run),'*preproc_bold.nii')));
-for i = 1:length(file_list)
-    sublist{i} = file_list{i}(98:102);
-end
+% List of files corresponding to the specific run and session
+file_list = filenames(fullfile(directories, strcat('*/ses-', num2str(ses), '/func/sub*chatroom_run-0', num2str(run), '*preproc_bold.nii')));
+sublist = cellfun(@(x) x(98:98+ID_length-1), file_list, 'UniformOutput', false);
 
-%keyboard
+% Initialize the list of new subjects to be processed
+new_list = {};
+
 if overwrite == 0
-    smooth_list = filenames(fullfile(directories,strcat('*/ses-',num2str(ses),'/func/ssub*chatroom_run-0',num2str(run),'*preproc_bold.nii')));
-    counter = 1;
-    for sub = 1:length(sublist)
-        curr_sub = sublist(sub);
-        if sum(contains(smooth_list,curr_sub)) == 0 
-            new_list(counter) = sublist(sub);
-            counter = counter + 1;
-        else
-            continue
-        end
-    end
+    % List of already smoothed subjects
+    smooth_list = filenames(fullfile(directories, strcat('*/ses-', num2str(ses), '/func/ssub*chatroom_run-0', num2str(run), '*preproc_bold.nii')));
+    
+    % Identify unsmoothed subjects
+    new_list = sublist(~ismember(sublist, cellfun(@(x) x(98:98+ID_length-1), smooth_list, 'UniformOutput', false)));
+else
+    % If overwriting, process all subjects
+    new_list = sublist;
 end
 
-% Run/submit batch script
 keyboard
+% Run/submit batch script for each subject in new_list
 cd(scriptdir)
 for sub = 1:length(new_list)
     PID = new_list{sub};
-%     ses = 2;
-%     run = 2;
-%    overwrite = 0;
-%     single_sub_smooth(PID,ses,run,overwrite)
-
-        s = ['#!/bin/bash\n\n'...
-     '#SBATCH -A p31589\n'...
-     '#SBATCH -p short\n'...
-     '#SBATCH -t 00:20:00\n'...  
-     '#SBATCH --mem=30G\n\n'...
-     'matlab -nodisplay -nosplash -nodesktop -r "addpath(genpath(''' repodir ''')); single_sub_smooth(' PID ', ' num2str(ses) ',' num2str(run) ',' num2str(overwrite) '); quit"\n\n'];
+    
+    % Create the batch script for the current subject
+    script_name = sprintf('smoothing_script_%s.sh', PID);
+    s = sprintf(['#!/bin/bash\n\n'...
+                 '#SBATCH -A p31589\n'...
+                 '#SBATCH -p short\n'...
+                 '#SBATCH -t 00:20:00\n'...  
+                 '#SBATCH --mem=30G\n\n'...
+                 'matlab -nodisplay -nosplash -nodesktop -r "addpath(genpath(''%s'')); single_sub_smooth(''%s'', %d, %d, %d); quit"\n\n'], ...
+                 repodir, PID, ses, run, overwrite);
    
-     scriptfile = fullfile(scriptdir, 'smoothing_script.sh');
-     fout = fopen(scriptfile, 'w');
-     fprintf(fout, s);
-     
-     
-     !chmod 777 smoothing_script.sh
-     !sbatch smoothing_script.sh
-     
+    % Write and execute the batch script
+    scriptfile = fullfile(scriptdir, script_name);
+    fout = fopen(scriptfile, 'w');
+    fprintf(fout, '%s', s);
+    fclose(fout);
+    
+    system(['chmod 777 ', scriptfile]);
+    system(['sbatch ', scriptfile]);
 end
