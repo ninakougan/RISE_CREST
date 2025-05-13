98bf100f60c3e6141a9ccd16ecd03e973faeefdc
ninakougan <ninakougan@northwestern.edu>
2025-05-13 15:45:02 -0500

    Update first_level_batch.m

--- first_levels/first_level_batch.m
+++ first_levels/first_level_batch.m
@@ -4,10 +4,10 @@
 % clunky and IT WILL CHANGE EVERYTIME YOU MOVE FILES AROUND. But since
 % we're in BIDS format... Maybe I can actually make this more dynamic. 
 
-scriptdir = '/home/nck1870/repos/RISE_CREST/first_levels';
+scriptdir = '/home/nck1870/scripts/RISE_CREST/first_levels';
 basedir = '/projects/b1108/studies/rise/data/processed/neuroimaging';
 % What run of your task are you looking at?
-run = 2;
+run = 1;
 % What session appears in your raw filenames when in BIDS format?
 ses = 1;
 % Do you want to overwrite previously estimated first levels or just add to
@@ -15,13 +15,13 @@ ses = 1;
 overwrite = 0;
 
 % rest, consumption, anticipation
-contrast = 'outcome'; % anticipation, outcome, chatroom
+contrast = 'anticipation'; % anticipation, outcome, chatroom
 
 %%
 fnames = filenames(fullfile(basedir,strcat('/fmriprep/ses-',num2str(ses),'/smoothed_data/ssub*ses-',num2str(ses),'*mid*run-0',num2str(run),'*')));
 
 if overwrite == 0
-    fl_list = filenames(fullfile(basedir,'/august24_T1/fl/sub-*/',strcat('ses-',num2str(ses),'/'),contrast,strcat('run-0',num2str(run)),'SPM.mat'));
+    fl_list = filenames(fullfile(basedir,'/fl/sub-*/',strcat('ses-',num2str(ses),'/'),contrast,strcat('run-0',num2str(run)),'SPM.mat'));
     counter = 1;
     for sub = 1:length(fnames)
         
@@ -49,7 +49,7 @@ for sub = 1:length(new_list)
      '#SBATCH -p short\n'...
      '#SBATCH -t 00:20:00\n'...  
      '#SBATCH --mem=20G\n\n'...
-     'matlab -nodisplay -nosplash -nodesktop -r "addpath(genpath(''' repodir ''')); run_sub_firstlevel_outcome(' num2str(ids) '); quit"\n\n'];
+     'matlab -nodisplay -nosplash -nodesktop -r "addpath(genpath(''' repodir ''')); run_sub_firstlevel_anticipation(' num2str(ids) '); quit"\n\n'];
    
      scriptfile = fullfile(scriptdir, 'first_level_script.sh');
      fout = fopen(scriptfile, 'w');
